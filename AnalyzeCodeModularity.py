e79f7971e1e0202f771ae9475be379c25600c022
praveengadiyaram369 <praveengadiyaram@gmail.com>
2020-01-31 19:35:35 +0100

    added walk through repositories

--- AnalyzeCodeModularity.py
+++ AnalyzeCodeModularity.py
@@ -1,7 +1,10 @@
 
+def walk_through_repositories():
+    pass
+
 if __name__ == '__main__':
     print('Analyze Code Modularity Starts ... ')
 
-    
+    walk_through_repositories()
     print('Analyze Code Modularity Ends ... ')
 
