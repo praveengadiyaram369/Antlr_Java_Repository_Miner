74108aef9b33da68e9e283b83ac5196e95da96eb
praveengadiyaram369 <praveengadiyaram@gmail.com>
2020-03-12 14:43:25 +0100

    added git api changes

--- github-api.py
+++ github-api.py
@@ -1,18 +1,37 @@
 import sys
 from git import Repo
 
-COMMITS_TO_PRINT = 5
 
-def print_commit(commit):
-    print('----')
-    print(str(commit.hexsha))
-    print(commit.tree)
-    print("\"{}\" by {} ({})".format(commit.summary,
-                                     commit.author.name,
-                                     commit.author.email))
-    print(str(commit.authored_datetime))
-    print(str("count: {} and size: {}".format(commit.count(),
-                                              commit.size)))
+def print_commit(commit, repo_path):
+
+    try:
+        print('----')
+        print(str(commit.hexsha))
+        print(commit.stats.files)
+        changed_files = commit.stats.files
+
+        # for tree in commit.tree.trees:
+        #     for blob in commit.blobs:
+        #         print(blob)
+        #     print(blob.name)
+
+        for file_path_name in changed_files.keys():
+            changes = repo.git.execute(
+                        ['git', 'cat-file','233ca8063361cfe16a53abf3bbb0e47b0b4b38a7', file_path_name, repo_path])
+            print(changes)
+
+        
+        #print(commit.tree.blobs[0].data_stream.read())
+        print("\"{}\" by {} ({})".format(commit.summary,
+                                        commit.author.name,
+                                        commit.author.email))
+        print(str(commit.authored_datetime))
+        print(str("count: {} and size: {}".format(commit.count(),
+                                                commit.size)))
+    except ValueError as ve:
+        print("Value error:  " + repo_path + "   " + str(ve))
+    except Exception as e:
+        print("Unexpected error:  " + repo_path + "   " + str(e))
 
 def print_repository(repo):
     print('Repo description: {}'.format(repo.description))
@@ -29,11 +48,11 @@ if __name__ == "__main__":
     if not repo.bare:
         print('Repo at {} successfully loaded.'.format(repo_path))
         print_repository(repo)
-
+    
         # create list of commits then print some of them to stdout
-        commits = list(repo.iter_commits('master'))[:COMMITS_TO_PRINT]
+        commits = list(repo.iter_commits('master'))
         for commit in commits:
-            print_commit(commit)
+            print_commit(commit, repo_path)
             pass
     else:
         print('Could not load repository at {} :('.format(repo_path))
