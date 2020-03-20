51d266514a2715206e2d0767f551c77defd9a799
praveengadiyaram369 <praveengadiyaram@gmail.com>
2020-03-20 11:51:09 +0100

    Added code documentation and formatted with PEP-8

--- GenerateConsolidatedJson.py
+++ GenerateConsolidatedJson.py
@@ -1,11 +1,18 @@
 import json
 import glob
 
-result = []                                         #list that will store the results of all Json 
-for index, f in enumerate(glob.glob("Repository_Commit_Data/*_data.json")):                       #here you will loop over multiple Json files
-    with open(f, "r" ,encoding='utf-8', errors='ignore') as infile:   
-        print(f'Appending File Contents of ... {index+1}. {f}')                 
-        result.append(json.load(infile, strict=False)) 
+# _initializing the result list which holds the information of all repo json objects
+result = []
 
-with open("final_repo_data.json", "w") as outfile:    #filling the resultant file with Jason content
-     json.dump(result, outfile, indent=4)
+# _iterate through all the repository json files
+for index, f in enumerate(glob.glob("Repository_Commit_Data/*_data.json")):
+    with open(f, "r", encoding='utf-8', errors='ignore') as infile:
+        print(f'Appending File Contents of ... {index+1}. {f}')
+
+        # _append each json object to the list
+        result.append(json.load(infile, strict=False))
+
+# _write resulting json to a file
+with open("final_repo_data.json", "w") as outfile:
+    # _json dump to convert list of json objects to a valid json and write to a file
+    json.dump(result, outfile, indent=4)
